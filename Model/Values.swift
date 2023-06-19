b6bbf97d7f1b7a7fd0e58a3f003fac9fb3a0f42b
Akshai_dev <akshai.kunjumon@gmail.com>
2023-06-19 16:48:11 +0530

    Model files

---
+++ Model/Values.swift
@@ -0,0 +1,14 @@
+
+
+import Foundation
+struct Values : Codable {
+    let id : Int?
+    let name : String?
+    let image_url : String?
+    let image: String?
+    let actualPrice, offerPrice: String?
+    let offer: Int?
+    let isExpress: Bool?
+    let banner_url: String?
+    
+}
